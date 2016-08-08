ded92bf939bf3ab7d1654138c568de410af4c706
Colin Morris <colin.morris2@gmail.com>
2016-08-08 11:44:35 -0400

    WIP: refactoring dir structure

---
+++ app/zone/index.ts
@@ -0,0 +1,15 @@
+export * from './zone';
+export * from './zoneaction;
+export * from './zone.component';
+export * from './zones.component';
+export * from './zones.data';
+export * from './activezone.component';
+export * from './activezone.service';
+
+
+// TODO: Do all these modules need to be exported?
+/**
+export * from './verb';
+export * from './zones.constants';
+export * from './zones.data.defns';
+*/
