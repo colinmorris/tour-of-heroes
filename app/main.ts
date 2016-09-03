c46103cdc33958bdec25430fd9fb2597d90a4e08
Colin Morris <colin.morris2@gmail.com>
2016-09-03 14:23:44 -0400

    remove commented code

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
-//import { platformBrowser } from '@angular/platform-browser';
-//import { AppModuleNgFactory } from './app.module.ngfactory';
 import { AppModule } from './app.module';
 
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
-//platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
-
