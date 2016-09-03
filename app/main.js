1da41f32fd8beb51250eecea64a45f95d3717efb
Colin Morris <colin.morris2@gmail.com>
2016-09-03 01:02:07 -0400

    enable prod mode

--- app/main.js
+++ app/main.js
@@ -1,8 +1,10 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var core_1 = require('@angular/core');
 //import { platformBrowser } from '@angular/platform-browser';
 //import { AppModuleNgFactory } from './app.module.ngfactory';
 var app_module_1 = require('./app.module');
+core_1.enableProdMode();
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
 //# sourceMappingURL=main.js.map
